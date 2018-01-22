ee699cdefd691f774ac06a63532c6c792b77fb46
capslocktao <haitaocapslock@outlook.com>
2018-01-22 22:24:45 +0800

    完成获取收藏歌曲列表

--- client/src/containers/collect-songs/collect-songs.js
+++ client/src/containers/collect-songs/collect-songs.js
@@ -1,16 +1,56 @@
 import React, {Component} from 'react';
+import { Icon } from 'antd-mobile';
+import { Link,withRouter } from 'react-router-dom';
+import { getRoute } from '../../util/backTo';
+import { connect } from 'react-redux';
+import { getSongs } from '../../redux/personal.redux';
+import './collect-songs.less'
 
+@connect(
+    state=>state.personal,
+    { getSongs }
+)
 class CollectSongs extends Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+    componentDidMount(){
+        this.props.getSongs(this.props.match.params.id)
+    }
 
     render() {
         return (
             <div id="collect-songs">
+                <div className="top-header">
+                    <Link to={getRoute()} className="left">
+                        <Icon type="left"/>
+                        <span>
+                           返回
+                        </span>
+                    </Link>
+                    <div className="title">收藏的单曲</div>
+                    <div className="right">
+                        完成
+                    </div>
+                </div>
+                {
+                    this.props.songList?
+                        <div className="collect-songd-wrapper">
+                            {
+                                this.props.songList.map(v=>(
+                                    <div className="single-song">
+                                        <div className="left">
+                                            <div className="name">{v.name}</div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        :
+                        ""
+                }
 
-                
             </div>
         )
     }
--- client/src/redux/personal.redux.js
+++ client/src/redux/personal.redux.js
@@ -3,14 +3,18 @@
  */
 import axios from 'axios';
 const GET_SUMMARY = "GET_SUMMARY";
+const GET_SONGS = "GET_SONGS";
 const initialState = {
-    summary:""
+    summary:"",
+    songList:""
 };
 
 export function personal( state=initialState,action ) {
     switch (action.type){
         case GET_SUMMARY:
             return { ...state,summary:action.payload };
+        case GET_SONGS:
+            return { ...state,songList:action.payload };
         default:
             return state;
     }
@@ -22,13 +26,30 @@ function getSummaryAction(data) {
         payload:data
     }
 }
-
+function getSongsAction(data) {
+    return {
+        type:GET_SONGS,
+        payload:data
+    }
+}
+//获取基本信息
 export function getSummary(id) {
     return dispatch=>{
         axios.get(`/mock/personal${id}/summary.json`).then(res=>{
             let data = res.data;
             if(data.result){
                 dispatch(getSummaryAction(data.data))
+            }
+        })
+    }
+}
+//获取收藏的歌曲列表
+export function getSongs(id) {
+    return dispatch=>{
+        axios.get(`/mock/personal${id}/collectSongs.json`).then(res=>{
+            let data = res.data;
+            if(data.result){
+                dispatch(getSongsAction(data.data))
             }
         })
     }
